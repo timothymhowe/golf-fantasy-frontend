481e16c8445fff1a3dbe9a55746b8c84a154252e
Timothy Howe <tim@ailette.io>
2024-01-25 12:15:17 -0500

    build(husky): :construction_worker: Configure husky and semantic-release, migrate git hooks to husky
    Configured semantic release and husky to manage versioning.  Moved pre-commit git hook under the purview of husky.

---
+++ utils/husky/pre-commit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Navigate to the project directory
+cd "$(dirname "$0")/../.." || exit 1
+
+# debug, sending notif with current directory
+# pwd | terminal-notifier -message
+
+# Activate the virtual environment
+source src/api/env/bin/activate
+
+# Generate the requirements.txt file
+pip freeze > src/api/requirements.txt || exit 0
+
+# add requirements.txt to commit
+git add src/api/requirements.txt || exit 1
+
+# Deactivate the virtual environment
+deactivate
+
+# notify the user that the pre-commit script has successfully been run.
+if command -v terminal-notifier > /dev/null 2>&1; then
+    terminal-notifier -message "pre commit: updated requirements.txt"
+else
+    echo "terminal-notifier is not installed."
+fi
