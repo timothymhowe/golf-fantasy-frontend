b700247f9fdfac80dc5c3b70eac4c5fa574d96ad
Timothy Howe <tim@ailette.io>
2024-11-25 10:47:59 -0800

    ci(CI/CD): :bug: fix bug in pre-commit.sh, update next.config.mjs to utilize prod API

--- next.config.mjs
+++ next.config.mjs
@@ -18,10 +18,14 @@ const nextConfig = {
     async rewrites() {
         return [
             {
+
+                // this is for local development
+                // source: '/api/:path*',
+                // destination: 'http://127.0.0.1:5001/:path*',
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:5000/:path*',
+                destination: 'https://fantasy-golf-staging-371710577879.us-central1.run.app/:path*',
             },
-            // More rewrites...
+            //
         ]
     },
 }
--- utils/husky/pre-commit.sh
+++ utils/husky/pre-commit.sh
@@ -1,26 +1,26 @@
-#!/bin/sh
+!/bin/sh
 
 # Navigate to the project directory
 cd "$(dirname "$0")/../.." || exit 1
 
-# debug, sending notif with current directory
-# pwd | terminal-notifier -message
+# # debug, sending notif with current directory
+# # pwd | terminal-notifier -message
 
-# Activate the virtual environment
-source .venv/bin/activate
+# # Activate the virtual environment
+# source .venv/bin/activate
 
-# Generate the requirements.txt file
-pip freeze > src/api/requirements.txt || exit 0
+# # Generate the requirements.txt file
+# pip freeze > src/api/requirements.txt || exit 0
 
-# add requirements.txt to commit
-git add src/api/requirements.txt || exit 1
+# # add requirements.txt to commit
+# git add src/api/requirements.txt || exit 1
 
-# Deactivate the virtual environment
-deactivate
+# # Deactivate the virtual environment
+# deactivate
 
-# notify the user that the pre-commit script has successfully been run.
-if command -v terminal-notifier > /dev/null 2>&1; then
-    terminal-notifier -message "pre commit: updated requirements.txt" 
-else
-    echo "terminal-notifier is not installed."
-fi
+# # notify the user that the pre-commit script has successfully been run.
+# if command -v terminal-notifier > /dev/null 2>&1; then
+#     terminal-notifier -message "pre commit: updated requirements.txt" 
+# else
+#     echo "terminal-notifier is not installed."
+# fi
