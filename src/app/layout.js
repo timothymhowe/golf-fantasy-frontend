ee4b64b3f07c29ac0e6e427868cce079c7b53eb9
Timothy Howe <tim@ailette.io>
2024-12-21 11:27:23 -0800

    feat(layout): :sparkles: update metadata and improve layout styling
    - Changed application title from "pick.golf(beta)" to "pick.golf" for branding consistency.
    - Added viewport settings and improved apple web app metadata for better mobile experience.
    - Enhanced icon management by organizing favicon and apple-touch-icon entries.
    - Updated body class to include a background color for improved visual aesthetics.
    
    These changes collectively enhance the application's branding, mobile usability, and overall user interface.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import { ClientProviders } from "./components/client-providers";
 import { FirebaseProvider } from "./components/firebase-provider";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -8,13 +7,23 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "pick.golf(beta)",
+  title: "pick.golf",
   description: "Show me what you got.",
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    viewportFit: 'cover',
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'pick.golf',
+  },
   openGraph: {
     title: 'Fantasy Golf',
     description: 'Show me what you got.',
     url: 'https://www.pick.golf',
-    siteName: 'Fantasy Golf',
+    siteName: 'www.pick.golf',
     images: [
       {
         url: '/og-image.png',
@@ -32,9 +41,15 @@ export const metadata = {
     images: ['/og-image.png'],
   },
   icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' },
+    ],
     other: [
-      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { rel: 'icon', url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
       { rel: 'icon', url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ]
@@ -44,11 +59,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black`}>
         <FirebaseProvider>
           <SpeedInsights />
           <Analytics />
-
           {children}
         </FirebaseProvider>
       </body>
